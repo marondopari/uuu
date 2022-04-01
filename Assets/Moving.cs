16bf4bc8a99ba17fa5ac4781a09a72330588793f
marondopari <mikaexo930@gmail.com>
2022-04-01 17:36:18 +0900

    uu

---
+++ Assets/Moving.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+        rb.AddForce(x, 0, z);
+    }
+}
