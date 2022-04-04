9909afb450757ffae5f83b7ed455cfbcf6ad9ba9
marondopari <mikaexo930@gmail.com>
2022-04-04 18:05:10 +0900

    スクリプト修正

--- Assets/Script/Moving.cs
+++ Assets/Script/Moving.cs
@@ -6,10 +6,10 @@ public class Moving : MonoBehaviour
 {
 
     public float speed = 6.0f;      //歩行速度
-    public float jumpSpeed = 8.0f;  //ジャンプ力
+    public float jumpSpeed = 8.0f;  //ジャンプ速度
     public float gravity = 20.0f;   //重力の大きさ
 
-    private Vector3 moveDirection = Vector3.zero;   //移動する方向とベクトル（動く力、速度）
+    private Vector3 moveVelocity = Vector3.zero;   //移動速度ベクトル
     private CharacterController controller;         //キャラクターコンポ―ネント用の変数
 
     void Start()
@@ -22,18 +22,19 @@ public class Moving : MonoBehaviour
         //キャラクターコントローラーのついているこのオブジェクトが接地していたら
         if (controller.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));  //左右上下のキー入力を取得し、移動量に代入
+
+            Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));  //左右上下のキー入力を取得し、移動方向に代入
             moveDirection = transform.TransformDirection(moveDirection);    //移動方向をローカルからワールド空間に変換する
-            moveDirection = moveDirection * speed;      //移動速度を掛ける
+            moveVelocity = moveVelocity * speed;      //移動速度を掛ける
 
             //スペースキーが押されていたら
             if (Input.GetButton("Jump"))
             {
-                moveDirection.y = jumpSpeed;           //Y軸方向への移動を追加する
+                moveVelocity.y = jumpSpeed;           //Y軸方向への移動を追加する
             }
         }
 
-        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);     //y軸方向への移動に重力を加える
-        controller.Move(moveDirection * Time.deltaTime);                    //キャラクターコントローラーを移動させる
+        moveVelocity.y = moveVelocity.y - (gravity * Time.deltaTime);     //y軸方向への移動に重力を加える
+        controller.Move(moveVelocity * Time.deltaTime);                    //キャラクターコントローラーを移動させる
     }
 }
