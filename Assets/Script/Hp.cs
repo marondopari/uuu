20ef5975308a62a3f49f12c419bd9b44a1165ad7
marondopari <mikaexo930@gmail.com>
2022-04-12 12:52:16 +0900

    スクリプト修正　Find()追加

--- Assets/Script/Hp.cs
+++ Assets/Script/Hp.cs
@@ -7,14 +7,16 @@ public class Hp : MonoBehaviour
 {
     private const int maxHp = 100;  //プレイヤーHP最大値100
     private int currentHp;          //現在のHP
-    public Slider slider;            //シーンに配置したSlider格納用
+    public Slider hpSlider;            //シーンに配置したSlider格納用
 
     // Start is called before the first frame update
     void Start()
     {
-        slider.maxValue = maxHp;    // Sliderの最大値をプレイヤーHP最大値と合わせる
+        hpSlider = GameObject.Find("Slider").GetComponent<Slider>();
+        hpSlider.maxValue = maxHp;    // Sliderの最大値をプレイヤーHP最大値と合わせる
+        
         currentHp = maxHp;      // 初期状態はHP満タン
-        slider.value = currentHp;	// Sliderの初期状態を設定（HP満タン）
+        hpSlider.value = currentHp;	// Sliderの初期状態を設定（HP満タン）
     }
     
     // 当たり判定
@@ -23,11 +25,11 @@ public class Hp : MonoBehaviour
         if (col.gameObject.name == "Enemy")    // エネミーとぶつかったとき
         {
             currentHp -= 10;        // 現在のHPを減らす
-            slider.value = currentHp;   // Sliderに現在HPを適用
-            Debug.Log("slider.value = " + slider.value);
+            hpSlider.value = currentHp;   // Sliderに現在HPを適用
+            Debug.Log("slider.value = " + hpSlider.value);
 
             // Sliderが最小値になったら（例：死んだら）
-            if (slider.value <= 0)
+            if (hpSlider.value <= 0)
             {
                 Destroy(gameObject);            // 物体を消去
                 Destroy(GameObject.Find("Slider")); // Sliderも消去
